df7a064500c3811dd07b32a4889fb219d46c5967
Hari Narayanan <harinarayanan4057@gmail.com>
2016-10-05 10:45:11 +0530

    Add files via upload

---
+++ evenorodd.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class evenorodd
+{
+	public static void main(String []args)
+	{
+		Scanner s= new Scanner(System.in);
+		System.out.println("Enter the number:\n");
+		int n =s.nextInt();
+		
+		if(n%2==0)
+			System.out.println("even\n");
+		else
+			System.out.println("odd\n");
+		}
+		
+	}
+	
+}
