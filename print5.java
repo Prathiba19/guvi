4e10c882917ec563d2974855c5ca0ab08c76debb
Hari Narayanan <harinarayanan4057@gmail.com>
2016-10-05 12:25:09 +0530

    Add files via upload

---
+++ print5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class print5
+
+{
+
+	public static void print()
+	{
+		for(int i=0;i<5;i++)
+		{
+			System.out.println("hello");
+		}
+
+	}
+
+	public static void main(String[] args) 
+	
+	{
+	
+		print();
+	}
+
+}
