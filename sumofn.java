c4a14ba6a2ee2b136853925d8d1b5f5141a0f23a
Hari Narayanan <harinarayanan4057@gmail.com>
2016-10-05 13:10:47 +0530

    Add files via upload

---
+++ sumofn.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class sumofn
+{
+	public static int Sum(int n)
+	{
+		int sum=0;
+		for(int i=1;i<=n;i++)
+		{
+			sum=sum+i;
+		}
+
+		return sum;
+	}
+	public static void main(String[] args) 
+	{	
+
+		System.out.println("enter the value of n:\n");
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int s = Sum(n);
+		System.out.println(s+" ");
+	}
+}
