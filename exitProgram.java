4b1cdeec4baaea7db88b4fca681a6348f95ba86f
Hari Narayanan <harinarayanan4057@gmail.com>
2016-10-27 19:52:23 +0530

    Add files via upload

---
+++ exitProgram.java
@@ -0,0 +1,24 @@
+import java.util.*;
+ 
+public class exitProgram
+{
+    public static void main(String[ ] args)
+    {
+      Scanner in = new Scanner(System.in);
+      System.out.println("Enter some strings !");
+      //List<String> list = new ArrayList<String>( );
+      boolean loop = true;
+        while(loop)
+        {
+          String s = in.nextLine( );
+ 
+            if(s.equals("")|s.equals("q"))
+            {
+              break;    
+            }
+            
+        }
+       
+    }//main ends
+ 
+}
