3ef700729d899c2afa27e1abf583853f059ca50b
Hari Narayanan <harinarayanan4057@gmail.com>
2016-11-01 12:07:58 +0530

    char and word count

--- charCount.java
+++ charCount.java
@@ -19,12 +19,34 @@ public class charCount
 		}
 
 
+		public static int countWord(String str)
+		{
+			int wordCount=1;
+			char[] ch1 = new char[str.length()];
+			ch1 = str.toCharArray();
+
+			for(int i=0;i<str.length();i++)
+			{
+				if(ch1[i]==' '&&ch1[i-1]!=' ')
+				{
+					wordCount++;
+				}
+
+			}
+
+			return wordCount;
+		}
+
+
+
+
 	public static void main(String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
 		String str = new String();
 		str =sc.nextLine();
 		System.out.println(countChar(str));
+		System.out.println(countWord(str));
 	
 	}
 }
