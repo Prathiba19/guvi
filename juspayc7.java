59000665e6b200c6bbd803a8b15995405a70f3ea
Hari Narayanan <harinarayanan4057@gmail.com>
2016-11-18 13:36:32 +0530

    Update juspayc7.java

--- juspayc7.java
+++ juspayc7.java
@@ -1,23 +1,3 @@
-/*import java.util.*;
-public class juspayc7 {
-	public static void main(String[] args)
-	{
-		int n=2;
-		String str="helloworld";
-		
-		//System.out.println(str.charAt(7));
-		//System.out.println(str.length());
-		StringBuffer sb=new StringBuffer(str);		
-		
-		for(int i=2;i<sb.length();)
-		{
-			sb.deleteCharAt(i);
-			n=n+1;
-			i=n;
-		}
-		System.out.println(sb.toString());
-		}
-}*/
 
 import java.util.*;
 
